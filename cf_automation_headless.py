31c907ada8abd0fc073624e5d06c7c2f78fcb627
Mohammad Solaiman <solaiman321@gmail.com>
2021-03-22 19:16:38 +0600

    done

--- cf_automation_headless.py
+++ cf_automation_headless.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 import pickle
 
 
@@ -310,8 +311,8 @@ def collect_all_link():
 
 def main():
     global total
-    # get_cookies()
-    login_with_cookies()
+    get_cookies()
+    # login_with_cookies()
     collect_all_link()
     print('collected all link')
     # try:

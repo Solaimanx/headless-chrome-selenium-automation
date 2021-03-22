fc0b016d9c9781e8df7049e1d023d9819869dcd5
Mohammad Solaiman <solaiman321@gmail.com>
2021-03-22 10:54:19 +0600

    done

--- cf_automation_headless.py
+++ cf_automation_headless.py
@@ -1,13 +1,15 @@
-
+import os
+import pyperclip
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import os
-from time import sleep
 
 
 
+# driver
 chrome_options = webdriver.ChromeOptions()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
 chrome_options.add_argument("--headless")
@@ -15,22 +17,25 @@ chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
 
+#code start here
 
 
-def scrape_top_news():
-    i = 0
-    while i < 5:
-        driver.get('https://fast.com')
-        sleep(9)
-        speed= driver.find_element_by_xpath('//*[@id="speed-value"]')
-        name= driver.find_element_by_xpath('//*[@id="speed-units"]')
-        speed_number = speed.text
-        speed_name = name.text
-        print(str(speed_number)+str(speed_name) + '  '+str(i))
-        i += 1
+driver.get('http://google.com/')
+search = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input')
+pyperclip.copy(' קונפידנס - לדבר אנגלית, לא לגמגם. ')
+search.click()
+sleep(2)
+search.send_keys(Keys.CONTROL,'v')
+sleep(3)
+search_button = driver.find_element_by_xpath('/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]')
+search_button.click()
+sleep(3)
+driver.save_screenshot("screenshot.png")
 
 
-scrape_top_news()
+
+
+
 
 
 
